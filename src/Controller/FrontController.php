0b4926f087acee4b08b6d77f2559a98fb5fd9b7e
Ahmedbenabdallah29435 <benabdallah.ahmed@esprit.tn>
2022-04-12 06:47:55 +0100

    crud + cs

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Evenement;
+use App\Form\EvenementFormType;
+use App\Repository\EvenementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/front", name="display_front")
+     */
+    public function index(): Response
+    {
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
+        ]);
+    }
+
+    /**
+     * @Route("/eventfront", name="ev_front_aff")
+     */
+
+    function afficher(EvenementRepository $rep)
+    {
+         $evenement = $rep->findall();
+         return $this->render('front/evenementsfront.html.twig', [
+             'tab' => $evenement
+         ]);
+
+    }
+   
+}
